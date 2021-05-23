e7f86c25a698cf1bdd5bfab00f1088c314edae04
rushikesh24 <zrushi24@gmail.com>
2021-05-24 02:06:12 +0530

    Submission folder removed

--- submission/code/1431.kids-with-the-greatest-number-of-candies.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        max_item = max(candies)
-        return [i + extraCandies >= max_item for i in candies]       
--- submission/code/1528.shuffle-string.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def restoreString(self, s: str, indices: List[int]) -> str:
-        ls = [''] * len(indices)
-        for i in range(len(ls)):
-            ls[indices[i]] = s[i]
-        return ''.join(ls)
