7a9b99e943ec98cbc185f1d954713f8ba8d7677d
SocioDroid <sociodroid2019@gmail.com>
2021-05-23 18:21:17 +0530

    Created using leetcode-cli

--- src/github.py
+++ src/github.py
@@ -30,17 +30,18 @@ def pushRepo(response):
         url = "https://{}:{}@github.com/{}/{}.git".format(response["owner"]["login"], getGithubToken(), response["owner"]["login"], response["name"])
         os.system("git remote set-url origin "+url)
         with open(".env", "a") as myfile:
-            myfile.write("GITHUB_REPO="+url)
+            myfile.write("\nGITHUB_REPO="+url)
     
     os.chdir(getSubmissionDirectory())
     os.system("git add -A")
     os.system("git commit -m 'Created using leetcode-cli'")
     os.system("git branch -M main")
     
-    # git remote set-url origin https://scuzzlebuzzle:<MYTOKEN>@github.com/scuzzlebuzzle/ol3-1.git
     os.system("git push -u origin main")
 
 def initGit():
     generateReadme()
-    response = createRepo()
+    response = None
+    if not checkRepoURL():
+        response = createRepo()
     pushRepo(response)
